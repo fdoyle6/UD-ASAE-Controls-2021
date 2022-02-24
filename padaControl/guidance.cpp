6250f749171899f60619fb3b0c3258c5116078cd
Frank Doyle <fdoyle@udel.edu>
2022-02-24 16:23:50 -0500

    Add blocking in gnc scripts

--- padaControl/guidance.cpp
+++ padaControl/guidance.cpp
@@ -1,1 +1,13 @@
 #include "padaGNC.hpp"
+
+// Pre-release functions
+
+
+// Target acquistion functions
+
+
+// Descent functions
+
+
+// Flare & land functions
+
--- padaControl/navigation.cpp
+++ padaControl/navigation.cpp
@@ -11,5 +11,3 @@
 
 // Flare & land functions
 
-
-// 
