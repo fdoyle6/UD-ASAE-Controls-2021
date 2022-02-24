d6e63a0647c55332c84be6a9469fc9eb7181258e
Frank Doyle <fdoyle@udel.edu>
2022-02-24 16:00:44 -0500

    start adding files

--- padaControl/gncMain.cpp
+++ padaControl/gncMain.cpp
@@ -1,7 +1,4 @@
-#include <iostream>
-#include "Eigen/Dense"
-#include "startup.hpp"
-#include ""
+#include "padaGNC.hpp"
 
 int main(void) {
 
---
+++ padaControl/padaGNC.hpp
@@ -0,0 +1,18 @@
+// Includes
+#include <iostream>
+#include "Eigen/Dense"
+#include "stateVars.cpp"
+
+// Defines 
+
+
+// startup.cpp prototypes
+
+
+// navigation.cpp prototypes
+
+
+// guidance.cpp prototypes
+
+
+// control.cpp prototypes
