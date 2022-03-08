1203c25677ed35a943fba0d0c632679052857120
Frank Doyle <fdoyle@udel.edu>
2022-03-08 16:04:14 -0500

    Copy-paste stuff from master branch

--- groundStationAndGUI/README.md
+++ groundStationAndGUI/README.md
@@ -1,3 +1,7 @@
 # Ground_Station
 
 This directory holds all of the files needed to run the ground station and gui. This, also, contains pictures artifacted from originally having a GPS Module on the main plane. Since, this has been scrapped, and the code functions without the GPS sending data, but we have not cleaned out the pictures. 
+
+Need to install PyQt5 and PyQt5 Designer
+Prob need to play around with numbers to get to fit screen. 
+
--- groundStationAndGUI/main.py
+++ groundStationAndGUI/main.py
@@ -14,29 +14,33 @@ from math import *
 import sys
 import os
 import io
-import serial.tools.list_ports
 import serial as sr
 import time
-from serial.threaded import LineReader, ReaderThread
 import csv
 
 #UI Files
 from LoadScreen import Ui_LoadScreen
 from DAS import Ui_DAS
+from DASnoGPS import Ui_MainWindow
 
 
 
 # Arguments !!! THESE MUST BE CHANGED WHEN RAN ON A DIFFERENT COMPUTER
 #upper left latitude and longitude of picture
-ul_lat_lon = (39.6816628374625, -75.75363767585272)#(32.61267,-97.48776) - map
+ul_lat_lon = (39.6816628374625, -75.75363767585272)#(39.68180233022854, -75.75196397747506)#(39.6816628374625, -75.75363767585272)-Benny;(32.61267,-97.48776) - map
 #lower right latitude and longitude of picture
-lr_lat_lon = (39.67454497595476, -75.74301612795641)#(32.60752,-97.48138) - map
+lr_lat_lon = (39.67454497595476, -75.74301612795641)#(39.68070781834817, -75.74960457883779)#(39.67454497595476, -75.74301612795641)-Benny;(32.60752,-97.48138) - map
 map =  'Benny.png'#path to picture
 chopHelper = 100 #varies depending on screen size
 bottom = 250 #varies depending on screen size
-COM = 'COM5' #varies depending on which COM port the GroundStation operates off of
-baud =9600
-
+COM = 'COM5' #'COM5' -Duino   'COM7' - Feather varies depending on which COM port the GroundStation operates off of
+baud =9600 #9600- Duino 115200 - Feather
+GPS_h_w = (1270,760) # I truly have no idea where these numbers come from, play around with new point statments until you find the lr corner
+playBackMode = input("Playback mode (Type yes/y or no/n): ") #true to read in last csv as if it were real time data
+if playBackMode == 'yes' or playBackMode =='y':
+    playBackMode = True
+else:
+    playBackMode = False
 
 def findImageSize(img_path):
     pil = Image.open(img_path)
@@ -48,7 +52,7 @@ def rollPitchYaw(magX,magY,magZ,accX,accY,accZ):
     mag_x = magX * cos(pitch) + magY * sin(roll) * sin(pitch) + magZ * cos(roll) * sin(pitch)
     mag_y = magY * cos(roll) - magZ * sin(roll)
     yaw = 180 * atan2(-mag_y, mag_x) / pi
-    return roll+180,pitch+180,yaw+180 #all angles between 0 and 360
+    return roll,pitch,yaw #all angles between 0 and 360
 
 def scale_to_img(lat_lon, h_w, ul_lat_lon, lr_lat_lon):
     """
@@ -61,10 +65,10 @@ def scale_to_img(lat_lon, h_w, ul_lat_lon, lr_lat_lon):
     # https://gamedev.stackexchange.com/questions/33441/how-to-convert-a-number-from-one-min-max-set-to-another-min-max-set/33445
     old = (lr_lat_lon[0], ul_lat_lon[0])
     new = (0, h_w[1])
-    y = ((lat_lon[0] - old[0]) * (new[1] - new[0]) / (old[1] - old[0])) + new[0]
+    y = ((lat_lon[0] - old[1]) * (new[1] - new[0]) / (old[0] - old[1]))
     old = (ul_lat_lon[1], lr_lat_lon[1])
     new = (0, h_w[0])
-    x = ((lat_lon[1] - old[0]) * (new[1] - new[0]) / (old[1] - old[0])) + new[0]
+    x = ((lat_lon[1] - old[0]) * (new[1] - new[0]) / (old[1] - old[0]))
     # y must be reversed because the orientation of the image in the matplotlib.
     # image - (0, 0) in upper left corner; coordinate system - (0, 0) in lower left corner
     return (int(x), int(y))
@@ -75,11 +79,18 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__()
         self.width = width
         self.height = height
-        self.main_ui = Ui_DAS()
+        self.main_ui = Ui_MainWindow()
         self.main_ui.setupUi(self)
         self.GPS = Widget(self.width,self.height)
-        self.DataHandler = DataHandler(self)
-        self.DataHandler.start()
+        if(playBackMode):
+            print("Rerunning last flight")
+            self.Playback = PlayBack(self)
+            self.Playback.start()
+        else:
+            print("Beginning new flight")
+            self.DataHandler = DataHandler(self)
+            self.DataHandler.start()
+            self.main_ui.StopButton.clicked.connect(self.DataHandler.stop)
 
 
 
@@ -93,7 +104,7 @@ class Widget(QWidget):
         self.image = QPixmap(map)
         self.setGeometry(width//2+chopHelper, chopHelper//2, 500, 300)
         self.resize(width // 2 - chopHelper, height - chopHelper//2)
-        self.show()
+        #self.show() #uncomment when GPS works
 
     #instantiates map
     def paintEvent(self, event):
@@ -115,8 +126,11 @@ class DataHandler(Thread):
     def __init__(self,ui):
         super().__init__()
         self.ui = ui
+        self.f = open('flight.csv', 'w+')
+
 
     def run(self):
+        writer = csv.writer(self.f)
         self.threadActive = True
         gs = sr.Serial(COM, baud)
         gs.reset_input_buffer()
@@ -125,17 +139,56 @@ class DataHandler(Thread):
             if(len(data)>4):
                 data_list = data.split(',')
                 if(len(data_list)==11):
+                    print(data_list)
+                    writer.writerow(data_list)
                     self.ui.main_ui.AltValue.setNum(max(round(float(data_list[0]),1),0))
                     if(data_list[10] == '0'):
                         self.ui.main_ui.PDHValue.setText(self.ui.main_ui.AltValue.text())
+                    else:
+                        self.ui.main_ui.PDHTitle.setStyleSheet("color: 'red'")
                     r,p,y = rollPitchYaw(float(data_list[1]),float(data_list[2]),float(data_list[3]),float(data_list[4]),float(data_list[5]),float(data_list[6]))
                     self.ui.main_ui.RollValue.setNum(int(r))
                     self.ui.main_ui.PitchValue.setNum(int(p))
                     self.ui.main_ui.YawValue.setNum(int(y))
                     if(float(data_list[9])>0.0):
                         self.ui.main_ui.VelValue.setNum(round(float(data_list[9]),1))
-                        x,y = scale_to_img((float(data_list[7]),float(data_list[8])),(self.ui.width // 2 - chopHelper, self.ui.height - chopHelper//2),ul_lat_lon,lr_lat_lon)
+                        x,y = scale_to_img((float(data_list[7]),float(data_list[8])),GPS_h_w,ul_lat_lon,lr_lat_lon)
                         self.ui.GPS.newPoint(x,y)
+
+    def stop(self):
+        self.threadActive = False
+        self.f.close()
+
+class PlayBack(Thread):
+    def __init__(self,ui):
+        super().__init__()
+        self.ui = ui
+        self.f = open('flight.csv', 'r')
+
+
+    def run(self):
+        reader = csv.reader(self.f)
+        for row in reader:
+            time.sleep(0.25)  # data rate is approximately 4 Hz
+            if len(row)>4:
+                data_list = row
+                print(data_list)
+                self.ui.main_ui.AltValue.setNum(max(round(float(data_list[0]),1),0))
+                if(data_list[10] == '0'):
+                    self.ui.main_ui.PDHValue.setText(self.ui.main_ui.AltValue.text())
+                else:
+                    self.ui.main_ui.PDHTitle.setStyleSheet("color: 'red'")
+                r,p,y = rollPitchYaw(float(data_list[1]),float(data_list[2]),float(data_list[3]),float(data_list[4]),float(data_list[5]),float(data_list[6]))
+                self.ui.main_ui.RollValue.setNum(int(r))
+                self.ui.main_ui.PitchValue.setNum(int(p))
+                self.ui.main_ui.YawValue.setNum(int(y))
+                if(float(data_list[9])>0.0):
+                    self.ui.main_ui.VelValue.setNum(round(float(data_list[9]),1))
+                    x,y = scale_to_img((float(data_list[7]),float(data_list[8])),GPS_h_w,ul_lat_lon,lr_lat_lon)
+                    self.ui.GPS.newPoint(x,y)
+            else:
+                pass
+        self.f.close()
 
 def main():
     os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"]='1'
@@ -144,7 +197,6 @@ def main():
     width,height = screen_resolution.width(), screen_resolution.height()
     win = MainWindow(width, height)
     win.show()
-    win.GPS.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
