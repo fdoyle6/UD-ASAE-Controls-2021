16a9b9ab48eee83211777c12c51c0f9f97a8f939
cczerwin762 <60986685+cczerwin762@users.noreply.github.com>
2022-05-03 16:35:22 -0400

    Add files via upload

--- DAS/DAS-Servo-Testing/DAS-Servo-Testing.ino
+++ DAS/DAS-Servo-Testing/DAS-Servo-Testing.ino
@@ -3,14 +3,18 @@
 Servo servoP; 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   servoP.attach(13);
-  servoP.writeMicroseconds(2000);
+  servoP.write(0);
   Serial.println("setup complete");
   delay(8000);
 }
 
 void loop() {
-  servoP.writeMicroseconds(1000);
+  servoP.write(0);
+  delay(1000);
+  servoP.write(90);
+  delay(1000);
+  Serial.println("spin");
 }
